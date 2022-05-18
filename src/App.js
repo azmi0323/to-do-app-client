90889900816b0b52809798d93cf979d84b9649a9
AzmiHabib <azmihabib0323@gmail.com>
2022-05-18 21:01:07 +0600

    commit after all route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+
 
 function App() {
   return (
     <div>
-     <h2>hello</h2>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Login.css'
+const Login = () => {
+    return (
+        <div>
+            this is login
+        </div>
+    );
+};
+
+export default Login;
