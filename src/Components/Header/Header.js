b2e9be0f16c2c80f4d77be2c50723a87d9a509de
AzmiHabib <azmihabib0323@gmail.com>
2022-05-18 22:11:58 +0600

    edit navber

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,91 @@
 import React from "react";
-import {
-  Container,
-  Nav,
-  Navbar,
-} from "react-bootstrap";
-import { Link } from "react-router-dom";
+import "./Header.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import './Header.css'
+import { NavLink } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import auth from "../../firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 const Header = () => {
+  const [user] = useAuthState(auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+    localStorage.removeItem('token')
+  };
+
   return (
-    <div>
-      <Navbar  bg="light" expand="lg">
-        <Container className="container" fluid>
-          <Navbar.Brand  href="#">Navbar scroll</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Link to="/">Home</Link>
-              <Link to="/workToDo">Work To Do</Link>
-              <Link to="/about">About</Link>
+    <div className="">
+      <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
+        <Container>
+          <NavLink  to="/">
+            <h6 className="h1 font-style text-dark fw-bold">
+              Todo<span className="text-warning">App</span>
+            </h6>
+          </NavLink>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "link-active" : "link"
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "link-active" : "link"
+                }
+                to="/workToDo"
+              >
+                Work To Do
+              </NavLink>
               
-              
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "link-active" : "link"
+                }
+                to="/about"
+              >
+                About
+              </NavLink>
             </Nav>
+            <Nav>
+              {user ? (
+                <div>
             
-            <Link to="/login" >
-                Login
-              </Link>
+                  <button
+                    className="btn btn-danger ms-3 fw-bold"
+                    onClick={handleSignOut}
+                  >
+                    Sign Out
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "link-active" : "link"
+                    }
+                    to="/login"
+                  >
+                    Login
+                  </NavLink>
+
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "link-active" : "link"
+                    }
+                    to="/signUp"
+                  >
+                    Sign Up
+                  </NavLink>
+                </div>
+              )}
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
